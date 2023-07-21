4fe04a7821505be4dc7da267e9b2ecac77e56003
vhenngames <37925345+davdwhyte87@users.noreply.github.com>
2023-07-21 03:17:16 +0100

    Update App.kt

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,7 @@
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpHandler
 import com.sun.net.httpserver.HttpServer
+import io.github.cdimascio.dotenv.Dotenv
 import src.blockchain.Node
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -45,8 +46,8 @@ internal object App {
     @JvmStatic
     fun main(args: Array<String>) {
 
-
-        val server = HttpServer.create(InetSocketAddress(8000), 0)
+        var dotenv: Dotenv = Dotenv.load()
+        val server = HttpServer.create(InetSocketAddress(dotenv.get("NODE_PORT", "3000").toInt()), 0)
         server.createContext("/test", MyHandler())
         server.createContext("/hello", HelloHandler())
         server.executor = null // creates a default executor
